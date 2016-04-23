5284f71c350be0ef57964aab5b8d89613cbc7e1a
Daniela Ivanova <danielaivanova_@abv.bg>
2016-04-24 02:43:12 +0300

    Add Exam 1@2February2015Morning

---
+++ Exam 1@2February2015Morning/SaddyKopper/SaddyKopper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+
+class SaddyKopper
+{
+    static void Main()
+    {
+        //read input
+        string strNum = Console.ReadLine();
+        int transCount = 0;
+
+
+        //logic
+        while (strNum.Length > 1 && transCount < 10)
+        {
+
+
+            BigInteger product = 1;
+
+            while (strNum.Length > 0)
+            {
+
+                strNum = strNum.Substring(0, strNum.Length - 1);
+
+                int sum = 0;
+
+                for (int i = 0; i < strNum.Length; i += 2)
+                {
+                    sum += (strNum[i] - '0');
+                }
+
+
+                product *= sum != 0 ? sum : 1;
+
+
+            }
+            transCount++;
+            strNum = product.ToString();
+        }
+
+        //print result
+        if (transCount < 10)
+        {
+            Console.WriteLine(transCount);
+        }
+        Console.WriteLine(strNum);
+    }
+}
+
